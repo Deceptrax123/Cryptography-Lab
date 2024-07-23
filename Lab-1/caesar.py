c7fd8110a6c20e3a2b62446df18991a183cb36ad
Deceptrax123 <srinitishsri@gmail.com>
2024-07-23 22:00:46 +0530

    add caesar and playfair

--- Lab-1/caesar.py
+++ Lab-1/caesar.py
@@ -1,33 +1,38 @@
-def encrypt(text,key):
-    res=""
+def encrypt(text, key):
+    res = ""
 
     for k in text:
         if k.isupper():
-            res+=chr((ord(k)+key-65)%26+65)
+            res += chr((ord(k)+key-65) % 26+65)
         elif k.islower():
-            res+=chr((ord(k)+key-97)%26+97)
+            res += chr((ord(k)+key-97) % 26+97)
         elif k.isdigit():
-            res+=chr((ord(k)+key-58)%10+48)
+            res += chr((ord(k)+key-58) % 10+48)
+        elif ord(k) == 32:
+            res += chr((ord(k)+key-32) % 1+32)
     return res
 
-def decrypt(env,key):
-    res=""
+
+def decrypt(env, key):
+    res = ""
     for k in env:
         if k.isupper():
-            res+=chr((ord(k)-key-65)%26+65)
+            res += chr((ord(k)-key-65) % 26+65)
         elif k.islower():
-            res+=chr((ord(k)-key-97)%26+97)
+            res += chr((ord(k)-key-97) % 26+97)
         elif k.isdigit():
-            res+=chr((ord(k)-key-48)%10+48)
+            res += chr((ord(k)-key-48) % 10+48)
+        elif ord(k) == 32:
+            res += chr((ord(k)-key-32) % 1+32)
     return res
 
 
-if __name__=='__main__':
-    message=input("Enter a message: ")
-    shift=4
+if __name__ == '__main__':
+    message = input("Enter a message: ")
+    shift = 4
 
-    res=encrypt(message,shift)
-    print("Original: ",res)
+    res = encrypt(message, shift)
+    print("Original: ", res)
 
-    original=decrypt(res,shift)
-    print("Decrypted: ",original)
+    original = decrypt(res, shift)
+    print("Decrypted: ", original)
