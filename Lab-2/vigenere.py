85cf9e73cac90b585b64c8d026d59f4ab5747afb
Deceptrax123 <srinitishsri@gmail.com>
2024-08-05 19:54:48 +0530

    add des

--- Lab-2/vigenere.py
+++ Lab-2/vigenere.py
@@ -8,19 +8,23 @@ def generate_key(msg, key):
             key.append(key[i % len(key)])
     return "".join(key)
 
+
 def encrypt_vigenere(msg, key):
     encrypted_text = []
     key = generate_key(msg, key)
     for i in range(len(msg)):
         char = msg[i]
         if char.isupper():
-            encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('A')) % 26 + ord('A'))
+            encrypted_char = chr(
+                (ord(char) + ord(key[i]) - 2 * ord('A')) % 26 + ord('A'))
         elif char.islower():
-            encrypted_char = chr((ord(char) + ord(key[i]) - 2 * ord('a')) % 26 + ord('a'))
+            encrypted_char = chr(
+                (ord(char) + ord(key[i]) - 2 * ord('a')) % 26 + ord('a'))
         else:
             encrypted_char = char
         encrypted_text.append(encrypted_char)
     return "".join(encrypted_text)
+
 
 def decrypt_vigenere(msg, key):
     decrypted_text = []
@@ -28,13 +32,16 @@ def decrypt_vigenere(msg, key):
     for i in range(len(msg)):
         char = msg[i]
         if char.isupper():
-            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('A'))
+            decrypted_char = chr(
+                (ord(char) - ord(key[i]) + 26) % 26 + ord('A'))
         elif char.islower():
-            decrypted_char = chr((ord(char) - ord(key[i]) + 26) % 26 + ord('a'))
+            decrypted_char = chr(
+                (ord(char) - ord(key[i]) + 26) % 26 + ord('a'))
         else:
             decrypted_char = char
         decrypted_text.append(decrypted_char)
     return "".join(decrypted_text)
+
 
 text_to_encrypt = "PAY MORE MONEY"
 key = "ABOVETHETIME"
